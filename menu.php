6c3dc79a43a0837426d4c551e66c2cae7d779627
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-11-25 13:42:11 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,9 +5,9 @@ if (isset($_COOKIE['c_user'])){
 	header('Location: ./menu.php');
 }
 if ($_COOKIE['c_user'] != "audit"){
-	header('Location: ./mainMenu.php');
+	header('Location: ./home.php');
 } else {
-	header('Location: ./viewerDr.php?show=CRT');
+	header('Location: ./menu.php?show=CRT');
 }
 
 ?>
